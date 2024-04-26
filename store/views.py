7d2b3076179ff85e403b8a9f143f9086a5f1997b
DariusPop16 <daryus_pop@yahoo.ro>
2024-04-26 17:03:19 +0300

    Delete Login

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from .models import Product, Category
-from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import get_user_model
@@ -11,12 +11,12 @@ def category_summary(request):
     return render(request, 'layout/category_summary.html', {"categories": categories})
 
 
-def product(request,pk):
+def product(request, pk):
     product: Product = Product.objects.get(id=pk)
-    return render(request, 'layout/product.html', {'product' : product})
+    return render(request, 'layout/product.html', {'product': product})
 
 
-def category(request,foo):
+def category(request, foo):
     foo = foo.replace('-', ' ')
     try:
         category = Category.objects.get(name=foo)
@@ -26,7 +26,7 @@ def category(request,foo):
         page_obj = paginator.get_page(page_number)
         return render(request, 'layout/category.html', {'products': page_obj, 'category': category})
     except:
-        messages.success(request, "That Category Doesn't Exist..." )
+        messages.success(request, "That Category Doesn't Exist...")
         return redirect('home')
 
 
@@ -48,14 +48,17 @@ def signup(request):
         username = request.POST.get('username')
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
-        phone_number = request.POST.get('phone_number')
         password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
 
         if User.objects.filter(email=email).exists():
             messages.error(request, 'Email is already taken')
             return redirect('signup')
+        if password != password_confirm:
+            messages.error(request, 'Passwords don\'t match')
+            return redirect('signup')
 
-        user = User.objects.create(
+        User.objects.create(
             email=email,
             username=username,  # Make sure username is set correctly based on your user model
             first_name=first_name,
@@ -74,24 +77,16 @@ User = get_user_model()  # Get the user model dynamically
 
 def user_login(request):
     if request.method == "POST":
-        email = request.POST.get('email')
+        username = request.POST.get('username')
         password = request.POST.get('password')
+        user = User.objects.get(username=username, password=password)
 
-        # Use the User model consistently
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            messages.error(request, 'Invalid Email')
-            return redirect('login')
-
-        # Correct usage of authenticate function
-
-        if user is None:
-            messages.error(request, 'Invalid Password')
-            return redirect('login')
-        else:
+        if user:
             login(request, user)
             return redirect('home')
+        else:
+            messages.error(request, 'Invalid Password or Username')
+            return redirect('login')
 
     return render(request, 'authentication/login.html')
 
@@ -99,3 +94,8 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('home')
+
+
+def navbar(request):
+    user_id = request.session.get('user_id')
+    return render(request, 'layout/navbar.html', {"user_id": user_id})
